6c8a7b93672721bf4e0f7ddbf1c9b25fb548a80c
Emil Gotsev <egotsev@gmail.com>
2018-03-12 15:50:03 +0200

    Firt java experience

---
+++ 11b/2018-02-12-first-java-exercise/src/org/elsys/model/Student.java
@@ -0,0 +1,18 @@
+package org.elsys.model;
+
+public class Student {
+
+	private String name;
+
+	public Student(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
