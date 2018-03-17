da94892296e095ebaaa940a21f69cf996dcb38c8
Emil Gotsev <egotsev@gmail.com>
2018-03-17 17:33:17 +0200

    Exercise on balls and boxes

---
+++ practice/05-balls-and-boxes/src/org/elsys/part1/Ball.java
@@ -0,0 +1,19 @@
+package org.elsys.part1;
+
+public class Ball {
+
+	public Ball(double volume, Color color) {
+	}
+
+	public Ball(double volume) {
+		
+	}
+
+	public double getVolume() {
+		return 0;
+	}
+
+	public Color getColor() {
+		return null;
+	}
+}
