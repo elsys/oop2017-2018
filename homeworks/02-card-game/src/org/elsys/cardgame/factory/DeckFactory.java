03441443b5ae365d2384d702d6c2541a61a31503
Emil Gotsev <egotsev@gmail.com>
2018-04-18 12:24:28 +0300

    New old card game hw

---
+++ homeworks/02-card-game/src/org/elsys/cardgame/factory/DeckFactory.java
@@ -0,0 +1,21 @@
+package org.elsys.cardgame.factory;
+
+import org.elsys.cardgame.api.Deck;
+
+public class DeckFactory {
+
+	public static Deck defaultWarDeck() {
+		// TODO
+		return null;
+	}
+
+	public static Deck defaultSantaseDeck() {
+		// TODO
+		return null;
+	}
+
+	public static Deck defaultBeloteDeck() {
+		// TODO
+		return null;
+	}
+}
