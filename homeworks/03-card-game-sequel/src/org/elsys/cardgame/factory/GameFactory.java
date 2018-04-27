7c50d70b8edff7b3e95475b09261bfb079d616a3
Emil Gotsev <egotsev@gmail.com>
2018-04-27 20:03:00 +0300

    Card game - second part

---
+++ homeworks/03-card-game-sequel/src/org/elsys/cardgame/factory/GameFactory.java
@@ -0,0 +1,24 @@
+package org.elsys.cardgame.factory;
+
+import java.util.List;
+
+import org.elsys.cardgame.api.Card;
+import org.elsys.cardgame.api.Game;
+
+public class GameFactory {
+
+	public static Game createWarGame(List<Card> cards) {
+		// TODO
+		return null;
+	}
+
+	public static Game createSantaseGame(List<Card> cards) {
+		// TODO
+		return null;
+	}
+
+	public static Game createBeloteGame(List<Card> cards) {
+		// TODO
+		return null;
+	}
+}
