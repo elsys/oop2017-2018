8701add5fb2297a27c982725372a68dc9ec74a76
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-05-03 11:07:13 +0300

    practice on annotations...

---
+++ practice/09-json-serializer/src/org/elsys/serializer/AbstractSerializer.java
@@ -0,0 +1,58 @@
+package org.elsys.serializer;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class AbstractSerializer implements Serializer {
+
+	protected List<Field> getFieldsToSerialize(Class<?> cls) {
+		List<Field> fields = new LinkedList<>();
+		while (cls != Object.class) {
+			fields.addAll(Arrays.asList(cls.getDeclaredFields()));
+			cls = cls.getSuperclass();
+		}
+		return fields;
+	}
+
+	protected boolean isArray(Object value) {
+		return value.getClass().isArray();
+	}
+
+	protected boolean isCollection(Object value) {
+		return value instanceof Collection;
+	}
+
+	protected boolean isDirectlySerializable(Object value) {
+		return value instanceof Boolean || value instanceof Number || value instanceof Character
+				|| value instanceof String;
+	}
+
+	@Override
+	public boolean areIncludedNullFields() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void includeNullFields(boolean includeNullFields) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isPretty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPretty(boolean pretty) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
